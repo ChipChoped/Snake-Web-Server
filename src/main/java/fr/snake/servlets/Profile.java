8fec1da5a6b22c0ecba946a1b0a4f408fe4df946
Chaïmaâ Touhami <chaimaa-touhami@outlook.fr>
2023-02-18 15:22:35 +0100

    Update Profile.java

--- src/main/java/fr/snake/servlets/Profile.java
+++ src/main/java/fr/snake/servlets/Profile.java
@@ -50,53 +50,6 @@ public class Profile extends HttpServlet {
 			this.getServletContext().getRequestDispatcher("/WEB-INF/page-not-found.jsp").forward(request, response);
 		}
 
-		/*ServletContext context = getServletContext();
-		String path = context.getInitParameter("path");
-
-		// Get requested image by path info.
-		String requestedImage = request.getPathInfo();
-
-		// Check if file name is actually supplied to the request URI.
-		if (requestedImage == null) {
-			// Do your thing if the image is not supplied to the request URI.
-			// Throw an exception, or send 404, or show default/warning image, or just ignore it.
-			response.sendError(HttpServletResponse.SC_NOT_FOUND); // 404.
-			return;
-		}
-
-		// Decode the file name (might contain spaces and on) and prepare file object.
-		File image = new File(path, "ChipChop.jpg");
-
-		// Check if file actually exists in filesystem.
-		if (!image.exists()) {
-			// Do your thing if the file appears to be non-existing.
-			// Throw an exception, or send 404, or show default/warning image, or just ignore it.
-			response.sendError(HttpServletResponse.SC_NOT_FOUND); // 404.
-			return;
-		}
-
-		// Get content type by filename.
-		String contentType = getServletContext().getMimeType(image.getName());
-
-		// Check if file is actually an image (avoid download of other files by hackers!).
-		// For all content types, see: http://www.w3schools.com/media/media_mimeref.asp
-		if (contentType == null || !contentType.startsWith("image")) {
-			// Do your thing if the file appears not being a real image.
-			// Throw an exception, or send 404, or show default/warning image, or just ignore it.
-			response.sendError(HttpServletResponse.SC_NOT_FOUND); // 404.
-			return;
-		}
-
-		// Init servlet response.
-		response.reset();
-		response.setContentType(contentType);
-		response.setHeader("Content-Length", String.valueOf(image.length()));
-
-		// Write image content to response.
-		Files.copy(image.toPath(), response.getOutputStream());
-
-		request.setAttribute("imagePath", path + "profil_pictures/" + username + ".jpg");*/
-
 		this.getServletContext().getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
 	}
 
