02fb8402d2dd6447beb6709f5a9d2bcb8bae0f86
Chaïmaâ Touhami <chaimaa-touhami@outlook.fr>
2023-02-18 15:22:04 +0100

    users table has now id as its primery key
    This enables users to change their username

--- init.sql
+++ init.sql
@@ -3,7 +3,8 @@ CREATE DATABASE IF NOT EXISTS snake DEFAULT CHARACTER SET utf8 COLLATE utf8_gene
 USE snake;
 
 CREATE TABLE users (
-    username VARCHAR(20) PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY ,
+    username VARCHAR(20),
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     email VARCHAR(50),
@@ -14,6 +15,6 @@ CREATE TABLE users (
     inscription_date DATE
 );
 
-INSERT INTO users VALUES
+INSERT INTO users (username, first_name, last_name, email, password, sex, victories, birth_date, inscription_date) VALUES
 ('TheUnknownMan', 'John', 'Doe', 'john-doe@outlook.fr',
     '1234azerty', 'M', 7, '1998-04-26', now())
