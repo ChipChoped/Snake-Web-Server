91a6ee8c68c1272ea239d378c241f402efecce88
Chaïmaâ Touhami <chaimaa-touhami@outlook.fr>
2023-02-24 19:24:39 +0100

    Communicates with the database

---
+++ src/main/java/fr/snake/services/UserService.java
@@ -0,0 +1,27 @@
+package fr.snake.services;
+
+import fr.snake.beans.BeanException;
+import fr.snake.dao.DAOException;
+import fr.snake.dao.DAOFactory;
+import fr.snake.dao.UserDAO;
+import fr.snake.dto.LoginsDTO;
+import fr.snake.dto.UserIDDTO;
+import fr.snake.functions.SHA256;
+
+import java.security.NoSuchAlgorithmException;
+
+public class UserService {
+    private static final DAOFactory daoFactory = DAOFactory.getInstance();
+    private static final UserDAO userDAO = daoFactory.getUserDao();
+
+    public static boolean doLoginsExist(LoginsDTO logins) throws DAOException, NoSuchAlgorithmException {
+        return userDAO.doLoginsExist(logins.getUsername(), logins.getPassword());
+    }
+
+    public static UserIDDTO getUserID(String username) throws DAOException, BeanException {
+        UserIDDTO userID = new UserIDDTO();
+        userID.setId(userDAO.getID(username));
+
+        return userID;
+    }
+}
